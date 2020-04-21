83c102e8c92ea8f48e8a96041dd72c8d1d74e583
cxtom <cxtom2008@gmail.com>
2020-04-21 12:17:25 +0800

    feat: support safe-html

--- src/compiler/modules/html.js
+++ src/compiler/modules/html.js
@@ -4,6 +4,17 @@
  */
 
 function postTransformNode(node) {
+
+    if (node.attrsMap && node.attrsMap['v-dangerous-html']) {
+        node.attrsMap['v-html'] = node.attrsMap['v-dangerous-html'];
+        delete node.attrsMap['v-dangerous-html'];
+    }
+
+    if (node.attrsMap && node.attrsMap['v-safe-html']) {
+        node.attrsMap['v-html'] = `_sf(${node.attrsMap['v-safe-html']})`;
+        delete node.attrsMap['v-dangerous-html'];
+    }
+
     if (node.type === 1 && node.attrsMap['v-html']) {
         const value = node.directives.find(d => d.name === 'html').value;
         delete node.attrsMap['v-html'];
