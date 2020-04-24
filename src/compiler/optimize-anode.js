071b918cd36f4e28374f0a1d7dbf47d40fbaef9f
cxtom <cxtom2008@gmail.com>
2020-04-24 10:26:59 +0800

    perf: optimize size of anode

--- src/compiler/optimize-anode.js
+++ src/compiler/optimize-anode.js
@@ -8,5 +8,25 @@ export default function optimize(aNode) {
     if (aNode.children) {
         aNode.children = aNode.children.map(optimize);
     }
+    if (aNode.props) {
+        aNode.props = aNode.props.map(prop => {
+            if (prop.raw) {
+                // 不能删除，运行时有用
+                prop.raw = 1;
+            }
+            if (prop.expr) {
+                delete prop.expr.raw;
+            }
+            return prop;
+        });
+    }
+    if (aNode.directives) {
+        Object.keys(aNode.directives).forEach(dir => {
+            delete aNode.directives[dir].raw;
+            if (aNode.directives[dir].value) {
+                delete aNode.directives[dir].value.raw
+            }
+        });
+    }
     return aNode;
 }
