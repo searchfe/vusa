12ac0c150f141a5e540eb82b4f5ceb4edaa8b780
hqq <hqqxxf@gmail.com>
2021-11-17 23:11:40 +0800

    test: add computed (#13)

--- src/shared/util.js
+++ src/shared/util.js
@@ -162,3 +162,10 @@ export const parseStyleText = cached(function (cssText) {
     });
     return res;
 });
+
+/**
+ * Perform no operation.
+ * Stubbing args to make Flow happy without leaving useless transpiled code
+ * with ...rest (https://flow.org/blog/2017/05/07/Strict-Function-Call-Arity/).
+ */
+export function noop() {}
