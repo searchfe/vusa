3ea81bc0bd5668451762eb15e50bb2f0b2945da0
donghualei <donghualei@baidu.com>
2021-11-24 22:14:28 +0800

    运行transition.spec.js

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,7 @@ module.exports = {
             setupFilesAfterEnv: [
                 '<rootDir>/test/helpers/wait-for-update.js',
                 '<rootDir>/test/helpers/trigger-event.js',
+                '<rootDir>/test/helpers/to-have-been-warned.js',
             ],
         },
     ],
