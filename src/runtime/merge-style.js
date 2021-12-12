0a1c5cd1ae218e0635a61473f3af9a6bf24aa320
donghualei <donghualei@baidu.com>
2021-12-12 14:02:21 +0800

    修复style渲染完为{color: [object,object]}的bug

--- src/runtime/merge-style.js
+++ src/runtime/merge-style.js
@@ -40,7 +40,11 @@ export default function (style, staticStyle, vShow) {
         if (Array.isArray(val)) {
             style[key] = val[val.length - 1];
         }
+        else if (isPlainObject(val) && Object.keys(val).length === 0) {
+            delete style[key];
+        }
     });
+
     return staticStyle
         ? extend(staticStyle, style)
         : style;
