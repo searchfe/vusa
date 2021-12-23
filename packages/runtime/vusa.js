7a0285fe7e3b0cb99cfba8f9cf32402600a1cdbf
donghualei <donghualei@baidu.com>
2021-12-23 16:52:34 +0800

    Revert "摘除修改生命周期的代码 (#46)"
    This reverts commit cb1085efc1d91a8a8e9a573116cd26c59259ee05.

--- packages/runtime/vusa.js
+++ packages/runtime/vusa.js
@@ -127,6 +127,20 @@
     }
 
     /**
+     * Quick string check - this is primarily used to tell
+     */
+    function isString(value) {
+        return value && Object.prototype.toString.call(value) === '[object String]';
+    }
+
+    /**
+     * Quick function check - this is primarily used to tell
+     */
+    function isFunction(value) {
+        return value && Object.prototype.toString.call(value) === '[object Function]';
+    }
+
+    /**
      * Check whether an object has the property.
      */
     var hasOwnProperty = Object.prototype.hasOwnProperty;
@@ -357,7 +371,7 @@
         created: 'inited',
         beforeMount: 'created',
         beforeDestroy: 'detached',
-        destroyed: 'disposed',
+        destroy: 'disposed',
         updated: 'updated',
         beforeUpdate: 'beforeUpdate',
         activated: 'activated',
@@ -471,11 +485,7 @@
             if (Array.isArray(val)) {
                 style[key] = val[val.length - 1];
             }
-            else if (isPlainObject$1(val) && Object.keys(val).length === 0) {
-                delete style[key];
-            }
         });
-
         return staticStyle
             ? extend(staticStyle, style)
             : style;
@@ -805,7 +815,7 @@
             enumerable: true,
             configurable: true,
             set: function set(newVal) {
-
+                
                 var value = getter ? getter.call(obj) : val;
                 if (newVal === value) {
                     return;
@@ -878,7 +888,40 @@
                 type: san.ExprType.ACCESSOR,
                 paths: ( ob.expr.paths ).concat( expr.paths),
             };
-            ob.context.set(finalExpr, value, {
+            ob.context.data.set(finalExpr, value, {
+                force: true,
+            });
+        }
+    }
+
+    function del(opt) {
+        var target = opt[0];
+        var key = opt[1];
+        opt.slice(2);
+
+        if (target === null) {
+            return;
+        }
+
+        var newTarget = Object.keys(target).reduce(function (pre, next) {
+            if (next !== key) {
+                pre[next] = target[next];
+            }
+            return pre;
+        }, {});
+
+        var ob = target.__ob__;
+
+        if (ob && Array.isArray(target)) {
+            // 暂不支持数组形式
+            return;
+        }
+        else if(ob) {
+            var finalExpr = {
+                type: san.ExprType.ACCESSOR,
+                paths: [].concat( ob.expr.paths ),
+            };
+            ob.context.data.set(finalExpr, newTarget, {
                 force: true,
             });
         }
@@ -1001,68 +1044,14 @@
      * @author cxtom(cxtom2008@gmail.com)
      */
 
-    function isInInactiveTree(vm) {
-        while (vm && (vm = vm.$parent)) {
-            if (vm._inactive) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function deactivateChildComponent(vm, direct) {
-        if (direct) {
-            vm._directInactive = true;
-            if (isInInactiveTree(vm)) {
-                return;
-            }
-        }
-        if (!vm._inactive) {
-            vm._inactive = true;
-            if (vm.children
-                && vm.children.length) {
-                for (var i = 0; i < vm.children.length; i++) {
-                    if (vm.children[i].nodeType === san.NodeType.CMPT) {
-                        deactivateChildComponent(vm.children[i]);
-                    }
-                }
-            }
-            vm._toPhase('deactivated');
-        }
-    }
-
-    function activateChildComponent(vm, direct) {
-        if (direct) {
-            vm._directInactive = false;
-            if (isInInactiveTree(vm)) {
-                return;
-            }
-        }
-        else if (vm._directInactive) {
-            return;
-        }
-
-        if (vm._inactive || vm._inactive === undefined) {
-            vm._inactive = false;
-            if (vm.children
-                && vm.children.length) {
-                for (var i = 0; i < vm.children.length; i++) {
-                    if (vm.children[i].nodeType === san.NodeType.CMPT) {
-                        activateChildComponent(vm.children[i]);
-                    }
-                }
-            }
-            vm._toPhase('activated');
-        }
-    }
-
     function createCallFactory(name) {
         return function call(direct) {
-            if (name === 'deactivited') {
-                deactivateChildComponent(this, direct);
+            var ele = this;
+            if (ele.nodeType === san.NodeType.CMPT) {
+                ele._toPhase(name);
             }
-            else {
-                activateChildComponent(this, direct);
+            if (ele.children && ele.children.length > 1) {
+                ele.children.forEach(call);
             }
         };
     }
@@ -1627,6 +1616,47 @@
     }
 
     /**
+     * @file s-watch-transition
+     * @author ngaiwe(ngaiwe@126.com)
+     */
+
+    function transitionHandler(handler, context) {
+    	if (isString(handler)) {
+    		return context && context['$options'] && context['$options']['methods'] && context['$options']['methods'][handler];
+    	}
+    	else if (isFunction(handler)) {
+    		return handler;
+    	}
+    	else {
+    		return ''
+    	}
+    }
+
+    function watcher(name, listener, context) {
+    	var watcher = {};
+    	// 第一步判断watcher类型
+    	if (isFunction(listener)) {
+    		watcher.handler = listener;
+    	}
+    	else if (isString(listener)) {
+    		watcher.handler = transitionHandler(listener, context);
+    	}
+    	else if (isPlainObject$1(listener)) {
+    		var handler = transitionHandler(listener.handler, context);
+
+    		if (listener.immediate) {
+    			handler.call(context, context.data.get(name));
+    		}
+
+    		watcher = Object.assign({}, listener, {
+    			handler: handler
+    		});
+    	}
+
+    	return watcher;
+    }
+
+    /**
      * @file component
      * @author cxtom(cxtom2008@gmail.com)
      */
@@ -1655,14 +1685,40 @@
         _h: toHtml,
         $emit: san.Component.prototype.fire,
         $on: san.Component.prototype.on,
-        $watch: san.Component.prototype.watch,
+        $off: function(name) {
+            var this$1$1 = this;
+
+            if (name) {
+                return san.Component.prototype.un.call(this, name);
+            }
+            if (this.listeners && Object.keys(this.listeners).length) {
+                Object.keys(this.listeners).forEach(function (l) {
+                    return san.Component.prototype.un.call(this$1$1, l)
+                });
+            }
+        },
+        $watch: function(name, listener, declaration) {
+            var this$1$1 = this;
+
+            if (!listener) { return; }
+
+            var source = undefined;
+
+            if (declaration && Object.keys(declaration) && !isPlainObject$1(listener)) {
+                source = Object.assign({}, declaration, {
+                    handler: listener
+                });
+            }
+
+            var ref = watcher(name, source? source : listener, this);
+            var handler = ref.handler;
+            
+            return san.Component.prototype.watch.call(this, name, function (newValue, sourceValue) { return handler.call(this$1$1, newValue, sourceValue.oldValue); });
+        },
         $nextTick: san.nextTick,
         $set: set,
         _da: changeDisabled,
-        $destroy: function () {
-            san.Component.prototype.dispose.call(this);
-            san.Component.prototype._leave.call(this);
-        },
+        $destroy: san.Component.prototype.dispose,
     };
     /* eslint-enable fecs-camelcase */
 
@@ -1691,12 +1747,6 @@
             return (children.concat(this.children)).filter(function (child) {
                 return child.nodeType === 5;
             });
-        },
-        _isMounted: function _isMounted() {
-            return !!this.lifeCycle.attached;
-        },
-        _isBeingDestroyed: function _isBeingDestroyed() {
-            return this.lifeCycle.detached !== true;
         },
         $root: function $root() {
             var root = this;
@@ -1858,7 +1908,10 @@
 
             if (options.watch && !optimizeSSR) {
                 Object.keys(options.watch).forEach(function (key) {
-                    this$1$1.watch(key, options.watch[key].bind(this$1$1));
+                    var ref = watcher(key, options.watch[key], this$1$1);
+                    var handler = ref.handler;
+
+                    this$1$1.watch(key, function (newValue, sourceValue) { return handler.call(this$1$1, newValue, sourceValue.oldValue); });
                 });
             }
 
@@ -1899,6 +1952,17 @@
 
             properties.$options = {
                 value: options,
+            };
+
+            properties.$delete = {
+                get: function get() {
+                    return function () {
+                        var opt = [], len = arguments.length;
+                        while ( len-- ) opt[ len ] = arguments[ len ];
+
+                        return del.call(me, opt);
+                    }
+                }
             };
 
             Object.defineProperties(this, properties);
@@ -1943,7 +2007,7 @@
                 : (options.data || {});
 
             if (!optimizeSSR) {
-                this._dataKeys = data && Object.keys(data) || [];
+                this._dataKeys = Object.keys(data) || [];
             }
 
             var initialData = extend({$style: {}}, defaultProps, data);
